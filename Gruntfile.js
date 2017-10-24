53a123e9fd5d0d77d9461fcaefaf6c2820063116
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-10-25 01:46:48 +0200

    setup coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,6 +155,7 @@ module.exports = function(grunt) {
 
 	// Dependencies
 	grunt.loadNpmTasks( 'grunt-qunit-istanbul' );
+	grunt.loadNpmTasks( 'grunt-coveralls' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
